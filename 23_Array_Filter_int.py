0675ac8132097c147e508cc62bace4f12f6154cf
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2025-06-23 23:51:54 +0600

    Create 23_Array_Filter_int.py

---
+++ 23_Array_Filter_int.py
@@ -0,0 +1,33 @@
+import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.speed(1)
+# t.color("red")
+# t.forward(400)
+# t.left(90)
+# t.forward(300)
+# t.left(135)
+# # t.forward(565)
+# # This code will draw a square and a triangle. The turtle will move 400 units in the forward direction, then turn left by 90 degrees to draw a square. Then it will turn left by 135 degrees to draw a triangle. Finally, it will move 565 units in the forward direction to complete the triangle. The turtle will stop moving after drawing the shapes.
+# i=0
+# t.left(85)
+# while True:
+#     t.right(170)
+#     t.forward(200)
+#     t.left(170)
+#     t.forward(200)
+#     t.pensize(i)
+#     t.speed(i)
+#     i += 0.5
+#     if i == 10:
+#         turtle.done()
+#         break
+
+# turtle.done()
+# # This code will draw a star using the turtle graphics library. The turtle will move in a zigzag pattern to draw the star shape. The turtle will stop moving after drawing the star.
+
+
+i= 10
+arr =[""]*i
+print(len(arr))
+print(arr)
