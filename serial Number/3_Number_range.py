d65c396f891d8b2da91d00d94449c15c9650fd5c
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-01-12 21:50:49 +0600

    remove range spaces

--- serial Number/3_Number_range.py
+++ serial Number/3_Number_range.py
@@ -3,8 +3,8 @@ num = input("Enter a number range (e.g., 10-20): ")
 
 arr = []
 print(num.split("-"))
-n1 = int(num.split("-")[0])
-n2 = int(num.split("-")[1])
+n1 = int(num.split("-")[0].strip())
+n2 = int(num.split("-")[1].strip())
 
 for i in range(n1, n2):
     arr.append(i)
