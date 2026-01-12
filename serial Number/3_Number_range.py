9206ba5b3219742015046b3a88693b2f387071a9
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-01-12 21:46:30 +0600

    Create 3_Number_range.py

---
+++ serial Number/3_Number_range.py
@@ -0,0 +1,12 @@
+# Input Number like 10-20
+num = input("Enter a number range (e.g., 10-20): ")
+
+arr = []
+print(num.split("-"))
+# n1 = int(num.split("-"))[0]
+# n2 = int(num.split("-"))[1]
+
+# for i in range(n1, n2):
+#     arr.append(i)
+
+# print(arr)
