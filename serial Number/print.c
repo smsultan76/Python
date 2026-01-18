014d76154118d504950897fcadb13976dbe9cc28
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-01-19 02:57:46 +0600

    added factorial

--- serial Number/print.c
+++ serial Number/print.c
@@ -1,5 +1,16 @@
 #include <unistd.h>
+int factorial(int n) {
+    if(n == 0 || n == 1) return 1;
+    return n * factorial(n-1);
+}
 int main(){
-    write(1, "Hello Bangladesh", 16);
+    write(1, "Hello Bangladesh.\n", 18);
+
+    int n = 5;
+    int fact = factorial(n);
+    char str[100];
+    sprintf(str, "Factorial of %d is %d\n", n, fact);
+    write(1, str, 22);
+
     return 0;
 }
